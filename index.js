f41c83f27903dc452dc63bce9f9ef9d10fa61524
Manon Sanoi <info@manonsanoi.com>
2023-03-16 19:15:39 +0700

    Make Change Client Database

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-const express           = require('express');
-const bodyParser        = require('body-parser');
-const cors              = require('cors');
-const dotenv            = require('dotenv');
-const axios             = require('axios');
-const mongoRoutes       = require('./routes/mongodb');
-const postgreRoutes     = require('./routes/postgresql');
-const mysqlRoutes       = require('./routes/mysql');
-const firestoreRoutes   = require('./routes/firestore');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const axios = require('axios');
+const { MongoClient } = require('mongodb'); // Import the MongoClient
+const setupRoutes = require('./routes');
 
 dotenv.config();
 
@@ -21,50 +19,19 @@ app.use((req, res, next) => {
 
 async function initializeApp() {
   try {
-    const response = await axios.get('https://global-config.sgp1.digitaloceanspaces.com/clientConfigs.json');
-    const clientConfigs = response.data;
+    const mongoClient = new MongoClient(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+    await mongoClient.connect();
+    const db = mongoClient.db('API');
+    const clientConfigs = await db.collection('clients').find().toArray();
+    await mongoClient.close();
 
-    function addHeaders(source, clientID) {
-      return (req, res, next) => {
-        res.setHeader('X-Source', source);
-        res.setHeader('X-ClientID', clientID);
-        next();
-      };
-    }
-
-    app.use('/api', (req, res, next) => {
-      const urlParts = req.url.split('/');
-      const clientToken = urlParts[1];
-      const clientConfigExists = Object.values(clientConfigs).some(config => config.clientToken === clientToken);
-
-      if (!clientToken) {
-        res.status(500).json({ message: 'Not authenticated client' });
-      } else if (!clientConfigExists) {
-        res.status(500).json({ message: 'Invalid client token' });
-      } else {
-        next();
-      }
-    });
-
-    for (const client in clientConfigs) {
-      const config = clientConfigs[client];
-
-      if (config.source === 'mongodb') {
-        app.use(`/api/${config.clientToken}`, addHeaders(config.source, config.clientId), mongoRoutes(config));
-      } else if (config.source === 'postgresql') {
-        app.use(`/api/${config.clientToken}`, addHeaders(config.source, config.clientId), postgreRoutes(config));
-      } else if (config.source === 'mysql') {
-        app.use(`/api/${config.clientToken}`, addHeaders(config.source, config.clientId), mysqlRoutes(config));
-      } else if (config.source === 'firestore') {
-        app.use(`/api/${config.clientToken}`, addHeaders(config.source, config.clientId), firestoreRoutes(config));
-      }
-    }
+    setupRoutes(app, clientConfigs);
 
     app.listen(process.env.PORT, () => {
       console.log(`Server is running on port ${process.env.PORT}`);
     });
   } catch (err) {
-    console.error('Failed to download clientConfigs.json', err);
+    console.error('Failed to fetch client configurations from MongoDB', err);
   }
 }
 
---
+++ routes/index.js
@@ -0,0 +1,41 @@
+function addHeaders(source, clientID) {
+  return (req, res, next) => {
+    res.setHeader('X-Source', source);
+    res.setHeader('X-ClientID', clientID);
+    next();
+  };
+}
+
+const sourceMap = {
+    mongodb: './mongodb',
+    postgresql: './postgresql',
+    mysql: './mysql',
+    firestore: './firestore',
+    api: './api'
+  };
+  
+  function setupRoutes(app, clientConfigs) {
+    app.use('/api', (req, res, next) => {
+      const urlParts = req.url.split('/');
+      const clientToken = urlParts[1];
+      const clientConfigExists = Object.values(clientConfigs).some(config => config.clientToken === clientToken);
+  
+      if (!clientToken) {
+        res.status(500).json({ message: 'Not authenticated client' });
+      } else if (!clientConfigExists) {
+        res.status(500).json({ message: 'Invalid client token' });
+      } else {
+        next();
+      }
+    });
+  
+    for (const client in clientConfigs) {
+      const config = clientConfigs[client];
+      const sourceFile = sourceMap[config.source];
+      const sourceRoutes = require(sourceFile);
+    
+      app.use(`/api/${config.clientToken}`, addHeaders(config.source, config.clientId), sourceRoutes(config));
+    }  
+  }
+  
+  module.exports = setupRoutes;  
