fb729b882577680cf3b93715e5c0e826a6548ef0
Manon Sanoi <info@manonsanoi.com>
2023-04-16 19:00:05 +0700

    Mongodb Count

--- routes/mongodb.js
+++ routes/mongodb.js
@@ -81,7 +81,32 @@ module.exports = function (clientConfig, connections) {
                 res.status(500).json({ message: err.message });
                 }
             });
-            
+
+            router.post(`/${item.clientToken}/:collection/count`, setCustomHeader, async (req, res) => {
+                const collectionName = req.params.collection;
+                const collection = db.collection(collectionName);
+                try {
+                  const { method, args } = req.body;
+                
+                  if (!method || !Array.isArray(args)) {
+                    res.status(400).json({ message: `Invalid request format` });
+                    return;
+                  }
+                
+                  if (method === `find`) {
+                    const count = await collection.countDocuments(args[0]);
+                    console.log("Count result:", count);
+                    res.status(200).json(count);
+                  } else {
+                    const result = await collection[method](...args).toArray();
+                    console.log("Query result:", result);
+                    res.status(200).json(result);
+                  }
+                } catch (err) {
+                  res.status(500).json({ message: err.message });
+                }
+            });
+              
             router.post(`/${item.clientToken}/:collection`, setCustomHeader, async (req, res) => {
                 const collectionName = req.params.collection;
                 const collection = db.collection(collectionName);
